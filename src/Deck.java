4bb9c915059fbfb4cbd68cea6908fdbd16d159ce
Matthew Henderson <matthenderson@hotmail.co.uk>
2018-01-13 18:22:21 +0000

    Slightly changed deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -26,16 +26,21 @@ public class Deck extends CardPile {
 	 */
 	public void dealCards(Player[] players) {
 		
+		//Counter for looping
 		int counter = 0;
 		
+		//Loop through the deck of cards
 		for (int i = 0; i < pile.size(); i++) {
 			
+			//Add the card to the players hand
 			players[counter].addToHand(pile.get(i));
 			
+			//If all players have been dealt to, start again at first player
 			if (counter >= players.length - 1) {
 				
 				counter = 0;
 			}
+			//Else increment counter
 			else {
 				
 				counter++;
