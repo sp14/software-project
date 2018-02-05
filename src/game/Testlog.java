397e1a07391bf5f07f132ac8884f5caf971e827b
Matthew Henderson <2031944H@AD.DCS.GLA.AC.UK>
2018-02-05 18:45:09 +0000

    updated testlog class

--- src/game/Testlog.java
+++ src/game/Testlog.java
@@ -8,7 +8,6 @@ public class Testlog {
 	
 	//Initialise the test log variables
 	private String filename = "toptrumps.log";
-	private boolean testlog = false;
 	private PrintWriter writer = null;
 	
 	public Testlog() {
@@ -20,11 +19,12 @@ public class Testlog {
 		
 		writer.print(description + ": ");
 		for (int i=0;i<deck.getPile().size();i++)
-			writer.print(" " + deck.getPile().get(i).getName() + " ");
+			writer.print(deck.getPile().get(i).getName() + " ");
 	}
 	
 	public void printCurrentCardsToLog(ArrayList<Player> players){
 		
+		writer.println("Cards in play");
 		for (int i = 0; i < players.size(); i++) {
 
 			writer.println(players.get(i).getCurrentCard());
@@ -39,7 +39,8 @@ public class Testlog {
 		//Loop through each player in the array list
 		for (int i = 0; i < players.size(); i++) {
 
-			writer.print(System.lineSeparator() + players.get(i) + ": ");
+			writer.println("players hands");
+			writer.println(players.get(i) + ": ");
 			for (int j=0; j<players.get(i).getHand().getPile().size();j++) {
 				//Prints the players hand to the test log file
 				writer.print(players.get(i).getHand().getPile().get(j).getName() + " ");
@@ -47,6 +48,10 @@ public class Testlog {
 		}
 	}
 	
+	/**
+	 * Prints a separator with the current round number into the log file
+	 * @param round - round number
+	 */
 	public void printRoundSeparator(int round) {
 		
 		writer.println("----------- Round" + round + " -----------");
